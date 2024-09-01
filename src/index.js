b182555a1d60c82b1b1d8cdca6158b20d4948954
Roman Toporkov <r.toporkov@ya.ru>
2024-09-01 18:11:41 +0300

    refactor: merge ConverterControl with Converter

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const MODE = process.env.NODE_ENV;
-
+const ENV_MODE = process.env.NODE_ENV;
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
 root.render(
-  <App mode={MODE}/>
+  <App envMode={ENV_MODE}/>
 );
